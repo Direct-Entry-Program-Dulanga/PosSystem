6f6d9c7acd731fb0b3492326e8e9d17cc40c9f84
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-05-05 23:02:55 +0530

    Add update commit

--- src/sample/Controller/OrderDetailsFormController.java
+++ src/sample/Controller/OrderDetailsFormController.java
@@ -1,13 +1,20 @@
 package sample.Controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import sample.Database.Database;
+import sample.Views.TM.OrderTM;
+import sample.model.Order;
 
 import java.io.IOException;
 
@@ -20,6 +27,53 @@ public class OrderDetailsFormController {
     public TableColumn colFinalCustomer;
     public TableColumn colFinalTotal;
 
+
+    public void initialize() {
+
+        colFinalOID.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colFinalODate.setCellValueFactory(new PropertyValueFactory<>("date"));
+        colFinalCustomer.setCellValueFactory(new PropertyValueFactory<>("customer"));
+        colFinalTotal.setCellValueFactory(new PropertyValueFactory<>("total"));
+
+        loadAllOrders();
+
+        //-----------------
+
+        tblOrder.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            FXMLLoader loader = new
+                    FXMLLoader(getClass().getResource("../Views/OrderItemForm.fxml"));
+            try {
+                Parent root = loader.load();
+                OrderItemFormController controller = loader.getController();
+                controller.setData(newValue.);
+                Stage s = new Stage();
+                s.setScene(new Scene(root));
+                s.show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        //-----------------
+
+    }
+
+    private void loadAllOrders() {
+        ObservableList<OrderTM> orderObList = FXCollections.observableArrayList();
+        for (Order o : Database.orderlist
+        ) {
+            orderObList.add(
+                    new OrderTM(o.getOrderId(),
+                            o.getOrderDate(),
+                            o.getOcustomerid(),
+                            o.getOtotalCost())
+            );
+        }
+        tblOrder.setItems(orderObList);
+    }
+
+
     public void backhome(ActionEvent actionEvent) throws IOException {
         Stage stage = (Stage) contextOrderForm.
                 getScene().getWindow();
@@ -28,4 +82,6 @@ public class OrderDetailsFormController {
                         .getResource
                                 ("../Views/DashBoardForm.fxml"))));
     }
+
+
 }
--- src/sample/Controller/OrderItemFormController.java
+++ src/sample/Controller/OrderItemFormController.java
@@ -1,4 +1,36 @@
 package sample.Controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import sample.Database.Database;
+import sample.model.ItemDetails;
+import sample.model.Order;
+
 public class OrderItemFormController {
+
+    public TableView<ItemDetails> tblOrderDetail;
+    public TableColumn colItemCode;
+    public TableColumn colQTY;
+    public TableColumn colUnitPrice;
+
+    public void initialize() {
+        colItemCode.setCellValueFactory(new PropertyValueFactory<>("code"));
+        colQTY.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        colUnitPrice.setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+    }
+
+    public void setData(String orderId) {
+        for (Order o : Database.orderlist
+        ) {
+            if (o.getOrderId().equals(orderId)) {
+                ObservableList<ItemDetails> detailsObList =
+                        FXCollections.observableArrayList(o.getItems());
+                tblOrderDetail.setItems(detailsObList);
+                return;
+            }
+        }
+    }
 }
